146d316e9da4078cf1a587075399c46b21abe759
Josh Hale <joshevanhale@gmail.com>
2025-10-27 19:35:14 -0400

    init commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+local motionModes = { "n", "x", "o" }
+local actionModes = { "x", "o" }
+local opts = { noremap = true, silent = true }
+
+map(motionModes, "j", "h", vim.tbl_extend("force", opts, { desc = "j → left" }))
+map(actionModes, "l", "j", vim.tbl_extend("force", opts, { desc = "l → down" }))
+map({ "n" }, "l", "gj", vim.tbl_extend("force", opts, { desc = "l → down" }))
+map(motionModes, ";", "l", vim.tbl_extend("force", opts, { desc = "; → right" }))
+map(motionModes, "h", ";", vim.tbl_extend("force", opts, { desc = "h → ;" }))
+
+map(motionModes, "<Up>", "<C-w>k", vim.tbl_extend("force", opts, { desc = "Window: focus up" }))
+map(motionModes, "<Down>", "<C-w>j", vim.tbl_extend("force", opts, { desc = "Window: focus down" }))
+map(motionModes, "<Left>", "<C-w>h", vim.tbl_extend("force", opts, { desc = "Window: focus left" }))
+map(motionModes, "<Right>", "<C-w>l", vim.tbl_extend("force", opts, { desc = "Window: focus right" }))
